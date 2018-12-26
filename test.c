7b0e9974d2b12d5f2c973f2715be0c05188e83b3
soyoon3292 <jsp0916@gmail.com>
2018-12-26 12:47:26 +0900

    Modified test.c.

---
+++ test.c
@@ -0,0 +1,17 @@
+// #include <stdio.h>
+
+// int main() {
+//     printf("Hello, World! \n");
+
+//     return 0;
+// }
+
+#include <stdio.h>
+
+int main() {
+    int a;
+    a = 10;
+    printf("a 의 값은 : $d \n", a);
+
+    return 0;
+}
